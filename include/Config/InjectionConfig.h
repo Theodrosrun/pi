0b6db0ca534591a47fa46b58ba5626e189143870
Theodros Mulugeta <theodros.mulugeta@runciter.ch>
2026-05-16 00:17:14 +0200

    feat order declaration

--- include/Config/InjectionConfig.h
+++ include/Config/InjectionConfig.h
@@ -21,25 +21,25 @@
 struct InjectionConfig {
     float TargetPressure_bar;  //!< Target pressure in bar
     float SafetyPressure_bar;  //!< Safety pressure limit in bar
-    float SyringeVolume_ml;    //!< Syringe volume in milliliters
-    float SyringeDiameter_mm;  //!< Syringe inner diameter in millimeters
-    float SyringeStroke_mm;    //!< Syringe piston stroke in millimeters
     float TargetFlow_ml_s;     //!< Target injection flow in milliliters per second
+    float SyringeVolume_ml;    //!< Syringe volume in milliliters
+    float SyringeStroke_mm;    //!< Syringe piston stroke in millimeters
+    float SyringeDiameter_mm;  //!< Syringe inner diameter in millimeters
 };
 
 //! \brief Left coronary injection configuration
 constexpr InjectionConfig C_LeftCoronaryInjectionConfig = {.TargetPressure_bar = 2.0f,
                                                            .SafetyPressure_bar = 2.0f,
+                                                           .TargetFlow_ml_s = 6.0f,
                                                            .SyringeVolume_ml = 30.0f,
-                                                           .SyringeDiameter_mm = 10.0f,
                                                            .SyringeStroke_mm = 100.0f,
-                                                           .TargetFlow_ml_s = 6.0f};
+                                                           .SyringeDiameter_mm = 10.0f};
 
 //! \brief Right coronary injection configuration
 constexpr InjectionConfig C_RightCoronaryInjectionConfig = {.TargetPressure_bar = 2.0f,
                                                             .SafetyPressure_bar = 2.0f,
+                                                            .TargetFlow_ml_s = 3.0f,
                                                             .SyringeVolume_ml = 30.0f,
-                                                            .SyringeDiameter_mm = 10.0f,
                                                             .SyringeStroke_mm = 100.0f,
-                                                            .TargetFlow_ml_s = 3.0f};
+                                                            .SyringeDiameter_mm = 10.0f};
 #endif  // INJECTIONCONFIG_H
